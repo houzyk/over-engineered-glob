e66f4810225007a2b172dd03d54d6cf48da078cf
Houzair Koussa <houzairmk@icloud.com>
2023-02-17 18:08:12 +0400

    [dev] ADD cf secret data

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  publicRuntimeConfig: {
+    CONTENTFUL_AUTH: process.env.CONTENTFUL_AUTH,
+    CONTENTFUL_PREVIEW_AUTH: process.env.CONTENTFUL_PREVIEW_AUTH,
+    CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
