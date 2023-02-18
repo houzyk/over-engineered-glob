cd4ba49e1894c684cf52280cf203fc4402ed45a6
Houzair Koussa <houzairmk@icloud.com>
2023-02-18 18:35:34 +0400

    [dev] REVERT lerna components

--- components/src/footer/tests/Footer.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from '@testing-library/react';
-import Footer from '../src/Footer';
-
-test('renders Footer Component', () => {
-  render(<Footer />);
-});
--- components/template/tests/Template.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from '@testing-library/react';
-import Template from '../src/Template';
-
-test('renders Template Component', () => {
-  render(<Template />);
-});
