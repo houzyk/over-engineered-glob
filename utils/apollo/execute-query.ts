fcaf21ef38578621a3d7e5a3eb18a92329d4a1f3
Houzair Koussa <houzairmk@icloud.com>
2023-02-18 23:16:20 +0400

    [chore][devops] FIX deployment issues

--- utils/apollo/execute-query.ts
+++ utils/apollo/execute-query.ts
@@ -2,15 +2,17 @@ import { DocumentNode } from "@apollo/client";
 import client from "./contentful-client";
 import { parse } from "graphql";
 
-const StringifyGQL = (gqlQuery: DocumentNode) : string => {
+const StringifyGQL = (gqlQuery: DocumentNode) => {
   return gqlQuery.loc && gqlQuery.loc.source.body;
 }
 
 const getDataFromCF = async (gqlQuery: DocumentNode) => {
   const entry = StringifyGQL(gqlQuery);
-  const query = parse(entry);
-  const data = await client.query({ query });
-  return data;
+  if (entry) {
+    const query = parse(entry);
+    const data = await client.query({ query });
+    return data;
+  }
 }
 
 export default getDataFromCF;
