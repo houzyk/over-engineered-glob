9f5d64416b71a1795e799c64d4c2a9e8bb5cbf1e
Houzair Koussa <houzairmk@icloud.com>
2023-02-19 23:25:30 +0400

    [dev] ADD styled components registry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import StyledComponentsRegistry from '../../lib/styled-components-registry';
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
